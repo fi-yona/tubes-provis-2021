18cc063d9162d65e259dfa4fe251f4f2088e3a92
Fiona Avila <fionaavila15@gmail.com>
2021-07-31 11:21:14 +0700

    Tambah form login kasus

--- src/kemahasiswaan_10119001_10119013/frm_login.java
+++ src/kemahasiswaan_10119001_10119013/frm_login.java
@@ -14,6 +14,8 @@ public class frm_login extends javax.swing.JFrame {
     /**
      * Creates new form frm_login
      */
+    
+    
     public frm_login() {
         initComponents();
     }
@@ -27,6 +29,15 @@ public class frm_login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        lbl_username = new javax.swing.JLabel();
+        lbl_password = new javax.swing.JLabel();
+        txt_username = new javax.swing.JTextField();
+        txt_password = new javax.swing.JTextField();
+        btn_log_in = new javax.swing.JButton();
+        btn_keluar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("LOGIN SIMULASI KASUS");
         addWindowListener(new java.awt.event.WindowAdapter() {
@@ -35,18 +46,126 @@ public class frm_login extends javax.swing.JFrame {
             }
         });
 
+        jPanel1.setBackground(new java.awt.Color(209, 209, 229));
+
+        jPanel2.setBackground(new java.awt.Color(162, 169, 226));
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED), "LOG IN", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Microsoft YaHei", 1, 12))); // NOI18N
+
+        lbl_username.setFont(new java.awt.Font("Microsoft YaHei", 1, 12)); // NOI18N
+        lbl_username.setText("USERNAME");
+
+        lbl_password.setFont(new java.awt.Font("Microsoft YaHei", 1, 12)); // NOI18N
+        lbl_password.setText("PASSWORD");
+
+        txt_username.setFont(new java.awt.Font("Microsoft YaHei", 0, 12)); // NOI18N
+
+        txt_password.setFont(new java.awt.Font("Microsoft YaHei", 0, 12)); // NOI18N
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(lbl_username)
+                        .addGap(18, 18, 18)
+                        .addComponent(txt_username))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(lbl_password)
+                        .addGap(15, 15, 15)
+                        .addComponent(txt_password, javax.swing.GroupLayout.DEFAULT_SIZE, 206, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbl_username)
+                    .addComponent(txt_username, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbl_password)
+                    .addComponent(txt_password, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        btn_log_in.setFont(new java.awt.Font("Microsoft YaHei", 1, 12)); // NOI18N
+        btn_log_in.setIcon(new javax.swing.ImageIcon(getClass().getResource("/kemahasiswaan_10119001_10119013/user2.png"))); // NOI18N
+        btn_log_in.setText("LOG IN");
+        btn_log_in.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btn_log_inMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btn_log_inMouseExited(evt);
+            }
+        });
+        btn_log_in.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_log_inActionPerformed(evt);
+            }
+        });
+
+        btn_keluar.setFont(new java.awt.Font("Microsoft YaHei", 1, 12)); // NOI18N
+        btn_keluar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/kemahasiswaan_10119001_10119013/keluar.png"))); // NOI18N
+        btn_keluar.setText("KELUAR");
+        btn_keluar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btn_keluarMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btn_keluarMouseExited(evt);
+            }
+        });
+        btn_keluar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_keluarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(82, 82, 82)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(134, 134, 134)
+                        .addComponent(btn_log_in)
+                        .addGap(18, 18, 18)
+                        .addComponent(btn_keluar)))
+                .addContainerGap(93, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(66, 66, 66)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btn_log_in)
+                    .addComponent(btn_keluar))
+                .addContainerGap(87, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
-        setSize(new java.awt.Dimension(416, 339));
+        setSize(new java.awt.Dimension(516, 339));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
@@ -55,6 +174,38 @@ public class frm_login extends javax.swing.JFrame {
         u.setVisible(true);
         dispose();
     }//GEN-LAST:event_formWindowClosing
+
+    private void btn_keluarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_keluarActionPerformed
+        frm_utama u = new frm_utama();
+        u.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_btn_keluarActionPerformed
+
+    private void btn_log_inActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_log_inActionPerformed
+        frm_kasus k = new frm_kasus();
+        k.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_btn_log_inActionPerformed
+
+    private void btn_log_inMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_log_inMouseEntered
+        btn_log_in.setBackground(new java.awt.Color(128,136,203));
+        btn_log_in.setForeground(new java.awt.Color(255, 255, 255));
+    }//GEN-LAST:event_btn_log_inMouseEntered
+
+    private void btn_log_inMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_log_inMouseExited
+        btn_log_in.setBackground(new java.awt.Color(255, 255, 255));
+        btn_log_in.setForeground(new java.awt.Color(0, 0, 0));
+    }//GEN-LAST:event_btn_log_inMouseExited
+
+    private void btn_keluarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_keluarMouseEntered
+        btn_keluar.setBackground(new java.awt.Color(255,102,102));
+        btn_keluar.setForeground(new java.awt.Color(255, 255, 255));
+    }//GEN-LAST:event_btn_keluarMouseEntered
+
+    private void btn_keluarMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_keluarMouseExited
+        btn_keluar.setBackground(new java.awt.Color(255, 255, 255));
+        btn_keluar.setForeground(new java.awt.Color(0, 0, 0));
+    }//GEN-LAST:event_btn_keluarMouseExited
 
     /**
      * @param args the command line arguments
@@ -92,5 +243,13 @@ public class frm_login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_keluar;
+    private javax.swing.JButton btn_log_in;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel lbl_password;
+    private javax.swing.JLabel lbl_username;
+    private javax.swing.JTextField txt_password;
+    private javax.swing.JTextField txt_username;
     // End of variables declaration//GEN-END:variables
 }
