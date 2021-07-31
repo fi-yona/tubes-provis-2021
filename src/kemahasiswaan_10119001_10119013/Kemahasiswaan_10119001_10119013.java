8dcdcb8af275794681b5633c729f91d9fd623ff4
Fiona Avila <fionaavila15@gmail.com>
2021-07-31 08:02:09 +0700

    Front-End Form Utama + Form Mahasiswa

---
+++ src/kemahasiswaan_10119001_10119013/Kemahasiswaan_10119001_10119013.java
@@ -0,0 +1,22 @@
+/*
+ * NAMA  : Fiona Avila Putri
+ * NIM   : 10119013
+ * KELAS : IF1 2019/2020
+ */
+package kemahasiswaan_10119001_10119013;
+
+/**
+ *
+ * @author Fiona Avila
+ */
+public class Kemahasiswaan_10119001_10119013 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frm_utama u = new frm_utama();
+        u.setVisible(true);
+    }
+    
+}
